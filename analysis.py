2e36e233305d662ecfb1cb81473b7574e5581629
chuckyee <chuckyee@physics.rutgers.edu>
2017-02-03 00:37:42 -0500

    Analysis of energy scatter / evolution for each group of 'eigen-structures'

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@
 from __future__ import division, print_function
 
 import glob
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from pymatgen.analysis.structure_matcher import StructureMatcher
@@ -49,23 +50,86 @@ def analyze_trnn2():
 
 
 def analyze_low_e_structs():
-    structs = ios.read_consolidated_poscars('data/gatheredPOSCARS')
+    # structs = ios.read_consolidated_poscars('data/gatheredPOSCARS')
     df = ios.read_individuals_file('data/Individuals')
+    E_vs_U = ios.read_E_vs_U()
+    with open('data/grouped-structs.pickle', 'rb') as f:
+        grouped_structs = pickle.load(f)
 
     energy_cutoff = 0.5         # eV / unit cell
     low_energy_IDs = df[df.Enthalpy < min(df.Enthalpy) + energy_cutoff].ID
     low_energy_IDs = ['EA{}'.format(id) for id in low_energy_IDs]
 
-    low_energy_structs = [s for s in structs if s.name in low_energy_IDs]
+    low_e_grouped_structs = [[s for s in group if s.name in low_energy_IDs]
+                             for group in grouped_structs]
+    low_e_grouped_structs = [group for group in low_e_grouped_structs if group]
 
-    sm = StructureMatcher()
-    grouped_low_energy_structs = sm.group_structures(low_energy_structs)
+    # low_energy_structs = [s for s in structs if s.name in low_energy_IDs]
+    # sm = StructureMatcher()
+    # grouped_low_energy_structs = sm.group_structures(low_energy_structs)
 
-    for struct_list in grouped_low_energy_structs:
-        IDs = [struct.name for struct in struct_list]
-        print("{:2} | {}".format(len(struct_list), ' '.join(IDs)))
+    # stem and leaf plot (of sorts)
+    for group in low_e_grouped_structs:
+        IDs = [struct.name for struct in group]
+        print("{:2} | {}".format(len(group), ' '.join(IDs)))
 
+    def compute_grouped_energies(U):
+        grouped_energies = []
+        for group in low_e_grouped_structs:
+            IDs = [struct.name for struct in group]
+            energies = [E_vs_U[ID]['E'][U] for ID in IDs if ID in E_vs_U]
+            if energies:
+                grouped_energies.append(energies)
+        return grouped_energies
+
+    def concatenate_groups(grouped_energies):
+        xx = []
+        yy = []
+        for i,EE in enumerate(grouped_energies):
+            xx += [i]*len(EE)
+            yy += EE
+        return np.asarray(xx), np.asarray(yy)
+
+    plt.figure()
+    ii = range(4)
+    E0s = [min(v['E'][i] for v in E_vs_U.values()) for i in ii]
+    for i in ii:
+        xx, yy = concatenate_groups(sorted(compute_grouped_energies(i)))
+        offset = 50 * i
+        plt.plot(xx, yy - E0s[i] + offset,
+                 'o', alpha = 0.5, label = 'U = {}eV'.format(i))
+
+    plt.legend()
+    plt.xlabel("Structure group")
+    plt.ylabel("Total energy (meV/atom, offset arb.)")
+    plt.title("Energy scatter within each group of structures")
+    plt.savefig("results/energy-scatter.pdf", bbox_inches = "tight")
+
+    # choose representative ID for each structure group
+    eigen_IDs = [[s.name for s in group if s.name in E_vs_U]
+                 for group in low_e_grouped_structs]
+    eigen_IDs = [group[0] for group in eigen_IDs if group]
+    print("\nEigen-IDs: {}".format(' '.join(eigen_IDs)))
+
+    plt.figure(figsize = (6,6))
+    gsID = "EA573"              # ID of experimental ground state
+    def E0(U):
+        i = E_vs_U[gsID]['U'].index(U)
+        return E_vs_U[gsID]['E'][i]
+
+    for k,v in E_vs_U.items():
+        if k not in eigen_IDs:
+            continue
+        xx = v['U']
+        yy = np.asarray(v['E']) - np.asarray([E0(U) for U in xx])
+        # yy = v['E']
+        plt.plot(xx, yy, 'o-', color = "blue", alpha = 0.5)
+
+    plt.xlim(-0.3, 3.3)
+    plt.xlabel("$U - J$ (eV)")
+    plt.ylabel("$E - E_0$ (meV/atom)")
+    plt.savefig("results/E-vs-U-eigenstructs.pdf", bbox_inches = "tight")
 
 if __name__ == '__main__':
-    # analyze_trnn2()
+    analyze_trnn2()
     analyze_low_e_structs()
