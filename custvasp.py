ddb9fc283e490aa3d868479c3675cb9e8bb0cc74
chuckyee <chuckyee@physics.rutgers.edu>
2017-03-02 12:48:47 -0500

    Bugfix jobs needs to be list

--- custvasp.py
+++ custvasp.py
@@ -10,6 +10,6 @@ from custodian.vasp.jobs import VaspJob
 handlers = [VaspErrorHandler(), UnconvergedErrorHandler(),
             AliasingErrorHandler(), FrozenJobErrorHandler(),
             PositiveEnergyErrorHandler(), MeshSymmetryErrorHandler()]
-jobs = VaspJob(sys.argv[1:])
+jobs = [VaspJob(sys.argv[1:])]
 c = Custodian(handlers, jobs, max_errors=10)
 c.run()
